86a741c869c8a21176639b19433cc49c7bae5bc6
Piotr Kuźmiak <piotrkuzmiak@poczta.onet.pl>
2026-05-18 11:51:40 +0200

    🎨 Improving structure

--- models/staging/jaffle_shop/stg_jaffle_shop_customers.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop_customers.sql
@@ -3,4 +3,4 @@
         first_name,
         last_name
 
-    from raw.jaffle_shop.jaffle_shop_customers
+    from {{ source('jaffle_shop', 'customers') }}
--- models/staging/jaffle_shop/stg_jaffle_shop_orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop_orders.sql
@@ -4,4 +4,4 @@
         order_date,
         status
 
-    from raw.jaffle_shop.jaffle_shop_orders
+    from {{ source('jaffle_shop', 'orders') }}
